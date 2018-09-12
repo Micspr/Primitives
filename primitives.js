835da9ea349ccb8b5e9d82175548bc47f163edc6
Michael Spraggins <michael.c.spraggins@gmail.com>
2018-09-12 15:47:48 -0700

    File for Primitives Javascript

---
+++ primitives.js
@@ -0,0 +1,24 @@
+// console.log("I won't run!")
+// Single Line Comment
+/* Single or multi-line comment */
+// Booleans
+// true; false;
+// One number type: integers and floating-points are sub categories
+// Numbers are between -(2^53-1) and (2^53-1)
+// Integers are whole numbers: -3, 0, 4, 438, etc
+// Floating Points are decimals: -3.5, 0.2, 4.4, 438.7543123, etc
+console.log(Number.isInteger(4));
+console.log(Number.isInteger(4.1));
+// Number has 3 symbolic values: Infinity, -Infinity, and NaN
+console.log(Number.isFinite(100));
+console.log(Number.isFinite(Infinity));
+console.log(Number.isNaN(200));
+console.log(Number.isNaN(NaN));
+// isNaN will only return true if you provide it NaN
+console.log('john'.length);
+console.log('john'[0]);
+console.log('john'.substr(2));
+console.log('john'.substr(0, 2));
+console.log('john'.toUpperCase());
+console.log('JOHN'.toLowerCase());
+
---
+++ test.js
@@ -0,0 +1,2 @@
+var yes = "John";
+console.log(Number.isNaN(1));
